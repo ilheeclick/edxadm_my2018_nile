a13fa749989828974769d80e90eaf106a1dae043
kodongkyu <blackdark13@naver.com>
2018-07-23 17:54:27 +0900

    test

--- tracking_control/views.py
+++ tracking_control/views.py
@@ -98,7 +98,7 @@ def logfile_download(request, date):
         date_list.append(date.strftime("%Y%m%d"))
 
     for searchDate in date_list:
-        logFileDownload(searchDate, WEB1_HOST, WEB_LOG, LOCAL1_DIR, 1)
+        #logFileDownload(searchDate, WEB1_HOST, WEB_LOG, LOCAL1_DIR, 1)
         logFileDownload(searchDate, WEB2_HOST, WEB_LOG, LOCAL2_DIR, 2)
         # ------------------------------- 실제 서버 반영시 하단 내용으로 반영
         # logFileDownload(searchDate, WEB1_HOST, WEB1_LOG, LOCAL1_DIR)
@@ -156,7 +156,7 @@ def logFileDownload(search_date, host, log_dir, local_dir, web_server):
     for i in sorted(file_list):
         if re.search(date_compile, i) is not None:
             split_date = i.find('-20')
-            print 'file_list ---->  ', i
+            #print 'file_list ---->  ', i
             sfile = str(i)
             searchfile = sfile[split_date+1:split_date+9]
             if searchfile == search_date:
@@ -164,8 +164,11 @@ def logFileDownload(search_date, host, log_dir, local_dir, web_server):
                 callback_for_filename = functools.partial(my_callback, i)
                 # sftp.get(i, local_dir+sfile)
                 # sftp.get(i, local_dir+sfile, callback=callback_for_filename)
-                cmd = 'sshpass -p' + REAL_WEB1_PW + ' ssh -t ' + USER_NAME + '@' + host + ' ~/manage/track.sh ' + searchfile
-                os.system(cmd)
+                cmd = 'sshpass -p' + REAL_WEB1_PW + ' ssh -tt ' + USER_NAME + '@' + host + ' ~/manage/track.sh ' + searchfile
+                print 'cmd == > ', cmd
+
+		os.system(cmd)
+                print 'os check'
                 sftp.chdir(WEB_DOWNLOAD_PATH)
                 sftp.get(i, local_dir+sfile, callback=callback_for_filename)
     log_change(local_dir, CHANGE_DIR, search_date, web_server)
@@ -243,8 +246,10 @@ def log_change(path_dir, change_local, search_date, web_server):
                         joinname_idx = text[joinname_index + 16:]
                         joinname_change = joinname_idx.find(',')
                         joinname_pattern = joinname_idx[0:joinname_change - 3]
-                        print 'joinname_pattern ==================', joinname_pattern, '\n\n\n\n\n\n\n\n\n\n\n'
-                        r6 = re.compile(joinname_pattern)
+			print 'filename => ', log
+                        print 'joinname_pattern ==================', joinname_pattern, '\n\n\n\n'
+                        print 'joinname_idx ====> ', joinname_idx, '\n'
+                        #r6 = re.compile(joinname_pattern)
 
                         uniname_index = text.find('\"name\\\":')
                         uniname_idx = text[uniname_index + 11:]
@@ -257,7 +262,7 @@ def log_change(path_dir, change_local, search_date, web_server):
 
                         if pwd2_pattern != "":
                             data = data.replace(pwd2_pattern, '********')
-                        data = re.sub(r6, '******', data, count=1)
+                        data = data.replace(joinname_pattern, '******')
                         data = data.replace(uniname_pattern, "\"******\\\"")
 
                     output.write(data)
